e85bfb1d4ca7c74c0e5c09867be89d61c560ddc9
tbrooks <tbrooks321@yahoo.com>
2022-02-04 00:49:18 -0500

    wrote some more functions in the index pertaining to specific roles

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const createTeam 
-const path
-const fs
+// const createTeam;
+
 let teamArray = [];
 
 
@@ -14,17 +15,17 @@ function init() {
             {
                 type: 'input',
                 name: 'name',
-                message: 'What is the name of the team manager?'
+                message: 'What is the name of the team Manager?'
             },
             {
                 type: 'input',
                 name: 'id',
-                message: 'What is the manager employee id?'
+                message: 'What is the employee ID of the team Manager?'
             },
             {
                 type: 'input',
                 name: 'email',
-                message: 'What is the email of the team manager?'
+                message: 'What is the email of the team Manager?'
             },
             {
                 type: 'input',
@@ -37,17 +38,17 @@ function init() {
         let manager = new Manager(answers.name, answers.id, answers.email, answers.office);
         teamArray.push(manager);
         console.log(teamArray)
-        createTeam();
+        addMembers();
     })
     
     }
-    function createTeam() {
+    function addMembers() {
         inquirer.prompt([
             {
             type: 'list',
             name: 'options',
             message: 'What would you like to do next?',
-            choices: ['Create Engineer', 'Create Intern', 'Done']
+            choices: ['Create Engineer', 'Create Intern', 'Build Team']
             }
 
         ])
@@ -56,26 +57,86 @@ function init() {
                 createEngineer();
             }   
             if (answer.options === 'Create Intern'){
-                console.log('creating engineer')
+                createIntern();
             } 
-            if (answer.options === 'Done'){
-                console.log('creating engineer')
+            if (answer.options === 'Build Team'){
+                buildTeam();
             } 
         })
     }
 
     function createEngineer() {
-        console.log('good job')
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of the Engineer?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the employee ID of the Engineer?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the email of the Engineer?'
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is the GitHub username of the Engineer?'
+            }
+        ])
+        .then(engineerInfo => {
+            let engineer = new Engineer(engineerInfo.name, engineerInfo.id,
+                engineerInfo.email, engineerInfo.github);
+            teamArray.push(engineer);
+            addMembers();   
+        })
     }
 
     function createIntern() {
-
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of the Intern?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the employee ID of the Intern?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the email of the Intern?'
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is the school of the Intern?'
+            }
+        ])
+        .then(internInfo => {
+            let intern = new Intern (internInfo.name, internInfo.id,
+                internInfo.email, internInfo.github);
+            teamArray.push(intern);
+            addMembers();   
+        })
     }
 
-    function done() {
+    function buildTeam() {
         // write file myTeam.html
         // research writeToFile
         // pass template into writeToFile
+        console.log(`
+        =========================================
+        you did enough go eat some ice cream fatty
+        =========================================
+        `
+        )
     }
 
 
