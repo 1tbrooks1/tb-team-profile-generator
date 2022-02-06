6fc79998654275b3ee54604125f58c8d379db311
tbrooks <tbrooks321@yahoo.com>
2022-02-06 14:37:22 -0500

    added validation to questions

--- index.js
+++ index.js
@@ -1,48 +1,91 @@
+// modules
 const fs = require("fs");
 const inquirer = require("inquirer");
 
+// employee objects
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
+// link to create page
 const createTeam = require('./src/template.js');
 
+// array for team members
 let team = [];
 
 function init() {
   const createManager = () => {
-    inquirer
+    console.log(`
+    ==================
+      Create Manager 
+    ==================  
+    `)
+    return inquirer
       .prompt([
         {
           type: "input",
           name: "name",
           message: "What is the name of the team Manager?",
+          validate: nameInput => {
+            if (nameInput.length > 0 && isNaN(nameInput)) {
+              return true;
+            } else {
+              console.log(' Please enter a valid name for the Manager.');
+              return false;
+            }
+          }
         },
         {
           type: "input",
           name: "id",
           message: "What is the employee ID of the team Manager?",
+          validate: id => {
+            if(!isNaN(id)) {
+              return true;
+            } else {
+              console.log(' Please enter a valid number for the ID of the Manager.');
+              return false;
+            }
+          }
         },
         {
           type: "input",
           name: "email",
           message: "What is the email of the team Manager?",
+          validate: email => {
+             const validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+            if (validEmail) {
+              return true;
+            } else {
+              console.log(' Please enter a valid email address.');
+              return false;
+            }
+          }
         },
         {
           type: "input",
           name: "office",
           message: "What is the office number?",
+          validate: office => {
+            if(!isNaN(office)) {
+              return true;
+            } else {
+              console.log(' Please enter a valid number for the office number.');
+              return false;
+            }
+          }
         },
       ])
       .then((managerAns) => {
         const { name, id, email, office } = managerAns;
         const manager = new Manager (name, id, email, office);
         team.push(manager);
+        console.log('Manager successfully created!');
         addMembers();
       });
   }
   const addMembers = () => {
-    inquirer
+    return inquirer
       .prompt([
         {
           type: "list",
@@ -65,65 +108,143 @@ function init() {
   }
 
   const createEngineer = () => {
-    inquirer
+    console.log(`
+    ===================
+      Create Engineer 
+    ===================
+    `)
+    return inquirer
       .prompt([
         {
           type: "input",
           name: "name",
           message: "What is the name of the Engineer?",
+          validate: nameInput => {
+            if (nameInput.length > 0 && isNaN(nameInput)) {
+              return true;
+            } else {
+              console.log(' Please enter a name for the Engineer.');
+              return false;
+            }
+          }
         },
         {
           type: "input",
           name: "id",
           message: "What is the employee ID of the Engineer?",
+          validate: id => {
+            if(!isNaN(id)) {
+              return true;
+            } else {
+              console.log(' Please enter a valid number for the ID of the Engineer.');
+              return false;
+            }
+          }
         },
         {
           type: "input",
           name: "email",
           message: "What is the email of the Engineer?",
+          validate: email => {
+            const validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+           if (validEmail) {
+             return true;
+           } else {
+             console.log(' Please enter a valid email address.');
+             return false;
+           }
+         }
         },
         {
           type: "input",
           name: "github",
           message: "What is the GitHub username of the Engineer?",
+          validate: github => {
+            if(github.length > 0) {
+              return true;
+            } else {
+              console.log(' Please enter valid GitHub username.')
+              return false;
+            }
+          }
         },
       ])
       .then((engineerAns) => {
         const { name, id, email, github} = engineerAns
         const engineer = new Engineer(name, id, email, github);
         team.push(engineer);
+        console.log('Engineer successfully created!');
         addMembers();
       });
   }
 
   const createIntern = () => {
-    inquirer
+    console.log(`
+    =================
+      Create Intern 
+    =================  
+    `)
+    return inquirer
       .prompt([
         {
           type: "input",
           name: "name",
           message: "What is the name of the Intern?",
+          validate: nameInput => {
+            if (nameInput.length > 0 && isNaN(nameInput)) {
+              return true;
+            } else {
+              console.log(' Please enter a name for the Intern.');
+              return false;
+            }
+          }
         },
         {
           type: "input",
           name: "id",
           message: "What is the employee ID of the Intern?",
+          validate: id => {
+            if(!isNaN(id)) {
+              return true;
+            } else {
+              console.log(' Please enter a valid number for the ID of the Engineer.');
+              return false;
+            }
+          }
         },
         {
           type: "input",
           name: "email",
           message: "What is the email of the Intern?",
+          validate: email => {
+            const validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+           if (validEmail) {
+             return true;
+           } else {
+             console.log(' Please enter a valid email address.');
+             return false;
+           }
+         }
         },
         {
           type: "input",
           name: "school",
           message: "What is the school of the Intern?",
+          validate: schoolInput => {
+            if (schoolInput.length > 0 && isNaN(schoolInput)) {
+              return true;
+            } else {
+              console.log(' Please enter a valid school for the Intern.');
+              return false;
+            }
+          }
         },
       ])
       .then((internAns) => {
         const { name, id, email, school } = internAns
         const intern = new Intern(name, id, email, school)
         team.push(intern);
+        console.log('Intern successfully created!');
         addMembers();
       });
   }
